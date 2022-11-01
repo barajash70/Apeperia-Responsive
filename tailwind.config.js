92c1245d4c8e88d17e18dc59adc02e354134e3a9
Barajas <carmen-895@hotmail.com>
2022-10-31 21:14:46 -0500

    Se termino cards de planes

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
         cenizaclaro: "#F7F4F4",
         verde: "#035A76",
         gris: " #666666",
+        lila: "#690F8E",
+        rojointenso: "#B00024",
       },
       fontFamily: {
         monserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
