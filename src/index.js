96f62b1be2ad820fef389b4bb7fcfff07af63d64
Barajas <carmen-895@hotmail.com>
2022-11-03 19:24:13 -0500

    Finalizado Apeperia

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import blackberry from './assets/img/blackberry.png';
 import apple from './assets/img/apple.png';
 import windows from './assets/img/windowsphone.png';
 import videoFormacion from './assets/img/formacion-java.mp4';
+import apeperiaLogo from './assets/img/logo-apeperia.svg';
+import facebook from './assets/img/facebook.png';
+import twitter from './assets/img/twitter.png';
+import instagram from './assets/img/instagram.png';
 
 
 const logoApeperiaElement = document.getElementById("imgLogo");
@@ -44,6 +48,18 @@ windowsElement.src = windows;
 
 const videoFormacionElement = document.getElementById("formacionJava");
 videoFormacionElement.src = videoFormacion;
+
+const apeperiaLogoElement = document.getElementById("Apeperia");
+apeperiaLogoElement.src = apeperiaLogo;
+
+const facebookElement = document.getElementById("imgFacebook");
+facebookElement.src = facebook;
+
+const twitterElement = document.getElementById("imgTwitter");
+twitterElement.src = twitter;
+
+const instagramElement = document.getElementById("imgInstagram");
+instagramElement.src = instagram;
 
 
 
